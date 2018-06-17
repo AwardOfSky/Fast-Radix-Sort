b0a49ef5550eb4ca430ae8e32caa2da0e3725792
AwardOfSky <34893148+AwardOfSky@users.noreply.github.com>
2018-06-17 19:40:06 +0100

    v0.6
    Added faster sorting of negative integers if sorting the last byte.

--- radix.c
+++ radix.c
@@ -18,7 +18,7 @@ int main(int argc, char * argv []) {
 
     /* Initial variable declaration */
     srand(time(NULL));
-    clock_t start, end;
+    clock_t t;
     int size = 100000000;
     int num_max = 2147483647;
     int num_min = 0;
@@ -49,13 +49,14 @@ int main(int argc, char * argv []) {
     }
         
     /* Time sort function execution */
-    start = clock();
+    t = clock();
     int_radix_sort(a, size);
-    end = clock();
+    t = clock() - t;
+    double time = (double)t / CLOCKS_PER_SEC;
   
     /* Print results */
-    printf("\nRadix sort took %.6f seconds.\n[sorted %d numbers from %d to %d].\n",
-	   (double)(end - start) / CLOCKS_PER_SEC, size, num_min, num_max);
+    printf("\nRadix sort took %f seconds.\n[sorted %d numbers from %d to %d].\n",
+	   time, size, num_min, num_max);
 
     if(array_sorted(a, size) != 0){
 	printf("The array was sorted successfully!\n");
@@ -81,21 +82,17 @@ int array_sorted(int vector[], int size) {
 
 /*
   Integer Radix LSD sort. Stable and out-of-place.
-
   ---Parameters---
   
   vector[] - Pointer to the orginal array of integers
   size     - Size of the array to sort
  
  ---List of optimizations implemented---
-
   1 - Use of powers of 2 for the expoents and bucket size in order to use
       shift and bitwise operations (expoent = 8 in order to sort 1 byte per iteration).
       This works a lot like the American Flag algorithm used to sort strings.
-
   2 - Small preliminary check of the initial unsorted array to determine
       number of bytes to sort. Special useful in randomly shuffled arrays.
-
   3 - The indexes of the buckets express the amount of elements of that respective
       index in the original array. There is also a array of pointers so that
       each pointer has the adress in the helper array where the given offset
@@ -106,12 +103,10 @@ int array_sorted(int vector[], int size) {
       each iteration, the algorithm switches the purpose of these two arrays
       in order to reduce copying overhead (eventually correcting this at the
       end if it stops in a even number of bytes).
-
   5 - The algorithms is the same even if the original array contains integers
       of different signs. The only thing we have to do at the end is starting
       from the negative numbers instead (in case of different signs). This task
       has a relatively small overhead.
-
   6 - Neglecting the shift operation when sorting the first byte (equals >> 0).
       
   Adding to the main structural optimizations of the algorithm, there are a
@@ -124,6 +119,7 @@ void int_radix_sort(register int vector[], register const int size) {
 
     /* Support for variable sized integers without overflow warnings */
     const int MAX_UINT__ = ((((1 << ((sizeof(int) << 3) - 2)) - 1) << 1) + 1);
+    const int LAST_EXP__ = (sizeof(int) - 1) << 3;
     
     /* Define std preliminary, constrain and expression to check if all bytes are sorted */
 #define PRELIMINARY__ 100
@@ -175,8 +171,17 @@ void int_radix_sort(register int vector[], register const int size) {
 	++bucket[*n];							\
     }									\
     s = bb;								\
-    for(i = 0; i < 0x100; s += bucket[i++]) {				\
-	point[i] = s;							\
+    if(exp == LAST_EXP__) {						\
+	for(i = 128; i < 0x100; s += bucket[i++]) {			\
+	    point[i] = s;						\
+	}								\
+	for(i = 0; i < 128; s += bucket[i++]) {				\
+	    point[i] = s;						\
+	}								\
+    } else {								\
+	for(i = 0; i < 0x100; s += bucket[i++]) {			\
+	    point[i] = s;						\
+	}								\
     }									\
     for(s = vec, k = &vec[size]; s < k; ++s) {				\
 	*point[(*s shift) & 0xFF]++ = *s;				\
@@ -198,8 +203,8 @@ void int_radix_sort(register int vector[], register const int size) {
 
     /* In case the array has both negative and positive integers, find the      */
     /* index of the first negative integer and put it in the start of the array */
-    if(((*vector ^ vector[size - 1]) < 0 && !BYTE_IS_ODD__) ||
-       ((*b ^ b[size - 1]) < 0 && BYTE_IS_ODD__)) {
+    if(exp != (LAST_EXP__ + 8) && (((*vector ^ vector[size - 1]) < 0 && !BYTE_IS_ODD__) ||
+				   ((*b ^ b[size - 1]) < 0 && BYTE_IS_ODD__))) {
 	int offset = size - 1;
     	int tminusoff;
 
